1eef50370db1207c12256e766803be24b78a0321
Michael Reiter <michaelreiter@live.ca>
2016-11-16 16:54:37 -0800

    Add diskinfo skeleton

---
+++ Assignment 3/diskinfo.c
@@ -0,0 +1,39 @@
+/*
+	University of Victoria
+	Fall 2016
+	CSC 360 Assignment 3
+	Michael Reiter
+	V00831568
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* ---------- Typedefs ---------- */
+
+typedef struct something {
+
+} something;
+
+/* ---------- Constants and global variables ---------- */
+
+#define TRUE 1
+#define FALSE 0
+
+/* ---------- Helper functions ---------- */
+
+
+/* ---------- Main ---------- */
+
+/*
+	Displays information about an input file system
+*/
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		printf("Error: use as follows diskinfo <file system image>\n");
+		exit(1);
+	}
+
+	return 0;
+}
