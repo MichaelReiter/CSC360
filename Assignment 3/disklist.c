72c3ed5383e161b2b2573313fed76f6628a829cd
Michael Reiter <michaelreiter@live.ca>
2016-11-19 20:46:53 -0800

    Update disklist skeleton

--- Assignment 3/disklist.c
+++ Assignment 3/disklist.c
@@ -1,7 +1,7 @@
 /*
 	University of Victoria
 	Fall 2016
-	CSC 360 Assignment 3
+	CSC 360 Assignment 3 Part 2
 	Michael Reiter
 	V00831568
 */
@@ -9,27 +9,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-/* ---------- Typedefs ---------- */
-
-typedef struct something {
-
-} something;
-
-/* ---------- Constants and global variables ---------- */
-
-#define TRUE 1
-#define FALSE 0
+#include <sys/mman.h>
+#include "a3helpers.h"
 
 /* ---------- Helper functions ---------- */
 
 /*
+	listing: an array of entities on disk to print
+	listingLength: the length of listing array
 	Prints the disk image directory listing
 */
-void printDirectoryListing() {
+void printDirectoryListing(disk_file_t* listing, int listingLength) {
 	// int i;
-	// for (i = 0; i < numFiles; i++) {
-	// 	printf("%s %10s %20s %s\n", fileType, fileSize, fileName, fileCreationDateTime);
+	// for (i = 0; i < listingLength; i++) {
+	// 	printf("%s %10d %20s %s\n", listing[i]->fileType, listing[i]->fileSize, listing[i]->fileName, listing[i]->fileCreationDateTime);
 	// }
 }
 
@@ -40,11 +33,12 @@ void printDirectoryListing() {
 */
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		printf("Error: use as follows diskinfo <file system image>\n");
+		printf("Error: use as follows disklist <file system image>\n");
 		exit(1);
 	}
 
-	printDirectoryListing();
+	disk_file_t* listing = malloc(listingLength * sizeof(disk_file_t));
+	printDirectoryListing(listing, listingLength);
 
 	return 0;
 }
