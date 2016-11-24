9c6839fb7bdf9e24a139eb71f6996c1f5b85a630
Michael Reiter <michaelreiter@live.ca>
2016-11-23 20:38:09 -0800

    Add file stretching

--- Assignment 3/diskget.c
+++ Assignment 3/diskget.c
@@ -8,7 +8,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -18,75 +17,39 @@
 /* ---------- Helper functions ---------- */
 
 /*
-	fileName: the file to check for
-	p: a pointer to the mapped memory
-	Returns TRUE if the file exists in the root directory p
-*/
-int fileExists(char* fileName, char* p) {
-	while (p[0] != 0x00) {
-		if ((p[11] & 0b00000010) == 0 && (p[11] & 0b00001000) == 0) {
-			char* currentFileName = malloc(sizeof(char));
-			char* currentFileExtension = malloc(sizeof(char));
-			int i;
-			for (i = 0; i < 8; i++) {
-				if (p[i] == ' ') {
-					break;
-				}
-				currentFileName[i] = p[i];
-			}
-			for (i = 0; i < 3; i++) {
-				currentFileExtension[i] = p[i+8];
-			}
-
-			strcat(currentFileName, ".");
-			strcat(currentFileName, currentFileExtension);
-
-			if (strcmp(fileName, currentFileName) == 0) {
-				return TRUE;
-			}
-
-			free(currentFileName);
-			free(currentFileExtension);
-		}
-		p += 32;
-	}
-	return FALSE;
-}
-
-/*
 	Reads a file from a disk image
 */
-void copyFile(char* source, char* destination, int* fileSize, int* firstLogicalSector, int physicalAddress) {
-	int count = 0;
-	int entry = *firstLogicalSector;
+void copyFile(char* p, char* file, int fileSize, int* firstLogicalSector, int physicalAddress) {
+	// int count = 0;
+	// int entry = *firstLogicalSector;
 
-	// copy the first 512 bytes from the first logical address
-	int i;
-	for (i = 0; i < SECTOR_SIZE; i++) {
-		if (count == *fileSize) {
-			return;
-		}
-		destination[count] = source[physicalAddress + i];
-		count++;
-	}
+	// // copy the first 512 bytes from the first logical address
+	// int i;
+	// for (i = 0; i < SECTOR_SIZE; i++) {
+	// 	if (count == *fileSize) {
+	// 		return;
+	// 	}
+	// 	destination[count] = source[physicalAddress + i];
+	// 	count++;
+	// }
 
-	// while the FAT table does not have an end of file value
-	// read in another 512 bytes from the physical address
-	while (getFatEntry(entry, source) != 0xFFF) {
-		// get the value from the next FAT entry
-		entry = getFatEntry(entry, source);
-		// do the necesary math to get the physical address from the logical address
-		physicalAddress = SECTOR_SIZE * (31 + entry);
+	// // while the FAT table does not have an end of file value
+	// // read in another 512 bytes from the physical address
+	// while (getFatEntry(entry, source) != 0xFFF) {
+	// 	// get the value from the next FAT entry
+	// 	entry = getFatEntry(entry, source);
+	// 	// do the necesary math to get the physical address from the logical address
+	// 	physicalAddress = SECTOR_SIZE * (31 + entry);
 
-		for (i = 0; i < SECTOR_SIZE; i++) {
-			// check to see if we have read all bytes from the input file
-			if (count == *fileSize) {
-				break;
-			}
-			destination[count] = source[physicalAddress + i];
-			count++;
-		}
-	}
+	// 	for (i = 0; i < SECTOR_SIZE; i++) {
+	// 		// check to see if we have read all bytes from the input file
+	// 		if (count == *fileSize) {
+	// 			break;
+	// 		}
+	// 		destination[count] = source[physicalAddress + i];
+	// 		count++;
+	// 	}
+	// }
 }
 
 /* ---------- Main ---------- */
@@ -110,20 +73,56 @@ int main(int argc, char* argv[]) {
 	fstat(fd, &buf);
 	char* p = mmap(NULL, buf.st_size, PROT_READ, MAP_SHARED, fd, 0);
 	if (p == MAP_FAILED) {
-		printf("Error: failed to map memory\n");
+		printf("Error: failed to map disk image memory\n");
 		exit(1);
 	}
 
-	if (!fileExists(argv[2], p + SECTOR_SIZE * 19)) {
+	int fileSize = getFileSize(argv[2], p + SECTOR_SIZE * 19);
+	if (fileSize > 0) {
+		// Open file and map memory
+		int fd2 = open(argv[2], O_RDWR | O_CREAT, 0666);
+		if (fd2 < 0) {
+			printf("Error: failed to open file\n");
+			exit(1);
+		}
+
+		int result = lseek(fd2, fileSize-1, SEEK_SET);
+		if (result == -1) {
+			close(fd2);
+			printf("Error: failed to seek to end of file");
+			exit(1);
+		}
+
+		result = write(fd2, "", 1);
+		if (result != 1) {
+			close(fd2);
+			printf("Error: failed to write last byte");
+			exit(1);
+		}
+
+		char* p2 = mmap(NULL, fileSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd2, 0);
+		if (p2 == MAP_FAILED) {
+			printf("Error: failed to map file memory\n");
+			exit(1);
+		}
+
+		// int i;
+		// for (i = 0; i < fileSize; i++) {
+		// 	printf("%c\n", p2[i]);
+		// }
+
+		// char* source;
+		// char* destination;
+		// int fileSize = getFileSize;
+		// int* firstLogicalSector;
+		// int physicalAddress;
+		// copyFile(source, destination, fileSize, firstLogicalSector, physicalAddress);
+
+		munmap(p2, fileSize);
+		close(fd2);
+	} else {
 		printf("File not found.\n");
 	}
-
-	// char* source;
-	// char* destination;
-	// int* fileSize;
-	// int* firstLogicalSector;
-	// int physicalAddress;
-	// copyFile(source, destination, fileSize, firstLogicalSector, physicalAddress);
 
 	// Clean up
 	munmap(p, buf.st_size);
