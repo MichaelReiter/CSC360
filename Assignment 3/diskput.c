cd4171f8ff9fea8a3e28e42707a8b710b4af6c1c
Michael Reiter <michaelreiter@live.ca>
2016-11-24 18:10:54 -0800

    Add diskput file opening

--- Assignment 3/diskput.c
+++ Assignment 3/diskput.c
@@ -8,31 +8,51 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "a3helpers.h"
 
 /* ---------- Helper functions ---------- */
 
 /*
-	Writes a file to an input file system image
+	p: a pointer to the mapped memory
+	p2: a pointer to the mapped file to write to
+	fileName: the file to copy from the disk image
+	Writes a file from a disk image to the local directory
 */
-void writeFile() {
-	// if (file not found) {
-	// 	printf("File not found.\n");
-	// }
+void copyFile(char* p, char* p2, char* fileName) {
+	// // Create entry in root directory
+	// // Correctly set entry datetime attribute
 
-	// if (insufficient space) {
-	// 	printf("Not enough free space in the disk image.\n");
-	// }
+	// // Copy each sector and update FAT table
 
-	// Copy each sector and update FAT table
+	// int firstLogicalSector = getFirstLogicalSector(fileName, p + SECTOR_SIZE * 19);
+	// int n = firstLogicalSector;
+	// int fileSize = getFileSize(fileName, p);
+	// int bytesRemaining = fileSize;
+	// int physicalAddress = SECTOR_SIZE * (31 + n);
+
+	// do {
+	// 	n = (bytesRemaining == fileSize) ? firstLogicalSector : getFatEntry(n, p);
+	// 	physicalAddress = SECTOR_SIZE * (31 + n);
+
+	// 	int i;
+	// 	for (i = 0; i < SECTOR_SIZE; i++) {
+	// 		if (bytesRemaining == 0) {
+	// 			break;
+	// 		}
+	// 		p2[fileSize - bytesRemaining] = p[i + physicalAddress];
+	// 		bytesRemaining--;
+	// 	}
+	// } while (getFatEntry(n, p) != 0xFFF);
 }
 
 /* ---------- Main ---------- */
 
 /*
-	Writes an input file from the current directory to an input file system image
+	Copies an input file from the current directory to an input file system image
 */
 int main(int argc, char* argv[]) {
 	if (argc < 3) {
@@ -50,16 +70,41 @@ int main(int argc, char* argv[]) {
 	fstat(fd, &buf);
 	char* p = mmap(NULL, buf.st_size, PROT_READ, MAP_SHARED, fd, 0);
 	if (p == MAP_FAILED) {
-		printf("Error: failed to map memory\n");
+		printf("Error: failed to map disk image memory\n");
+		close(fd);
 		exit(1);
 	}
 
-	// Write to disk image
-	writeFile();
+	// Open file and map memory
+	int fd2 = open(argv[2], O_RDWR);
+	if (fd2 < 0) {
+		printf("File not found.\n");
+		close(fd);
+		exit(1);
+	}
+	struct stat buf2;
+	fstat(fd2, &buf2);
+	int fileSize = buf2.st_size;
+	char* p2 = mmap(NULL, fileSize, PROT_READ, MAP_SHARED, fd2, 0);
+	if (p2 == MAP_FAILED) {
+		printf("Error: failed to map file memory\n");
+		exit(1);
+	}
+
+	// Copy file from local directory to disk image if there's space
+	int totalDiskSize = getTotalDiskSize(p);
+	int freeDiskSize = getFreeDiskSize(totalDiskSize, p);
+	if (freeDiskSize >= fileSize) {
+		copyFile(p, p2, argv[2]);
+	} else {
+		printf("Not enough free space in the disk image.\n");
+	}
 
 	// Clean up
 	munmap(p, buf.st_size);
+	munmap(p2, fileSize);
 	close(fd);
+	close(fd2);
 
 	return 0;
 }
