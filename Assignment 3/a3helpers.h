792b253160f2f7a4910c13d5631a2c9df5bc8827
Michael Reiter <michaelreiter@live.ca>
2016-11-26 17:22:16 -0800

    Remove unused header info

--- Assignment 3/a3helpers.h
+++ Assignment 3/a3helpers.h
@@ -1,18 +1,8 @@
-/* ---------- Typedefs ---------- */
-
-typedef struct disk_file_t {
-  char fileType;
-  int fileSize;
-  char* fileName;
-  char* fileCreationDateTime;
-} disk_file_t;
-
 /* ---------- Constants and global variables ---------- */
 
 #define TRUE 1
 #define FALSE 0
 #define SECTOR_SIZE 512
-#define LISTING_SIZE 512
 
 /* ---------- Helper functions ---------- */
 
